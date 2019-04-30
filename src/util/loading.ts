dcaa9828485f3b793f412186bb5d5cb1cea9e5d8
gcwlucky <138811699@qq.com>
2019-04-30 12:58:05 +0800

    feat: showLoading增加bar选项：导航栏loading态

--- src/util/loading.ts
+++ src/util/loading.ts
@@ -1,14 +1,19 @@
 function show(txt: boolean | string) {
-    wx.showToast({
-        title: typeof txt === 'boolean' ? '加载中' : txt,
-        icon: 'loading',
-        mask: true,
-        duration: 60000
-    })
+    if(typeof txt === 'string' && txt === 'bar') {
+        wx.showNavigationBarLoading({})
+    }else {
+        wx.showToast({
+            title: typeof txt === 'boolean' ? '加载中' : txt,
+            icon: 'loading',
+            mask: true,
+            duration: 60000
+        })
+    }
 }
 
 function hide() {
     wx.hideToast({});
+    wx.hideNavigationBarLoading({});
 }
 
 export default {
