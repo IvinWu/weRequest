eca5f07c5ba3d73b6bdfd05c3b2601db915c14da
jiebinliao <jiebinliao@tencent.com>
2021-06-03 13:36:08 +0800

    feat: 用户设置 catchError 时，逻辑错误会把返回数据一并抛出

--- src/module/catchHandler.ts
+++ src/module/catchHandler.ts
@@ -7,6 +7,16 @@ interface ThrowError {
     type: ThrowErrorType
     res: any
 }
+
+class ErrorWithData extends Error {
+    data: any;
+
+    constructor(msg: string, data: any = {}) {
+        super(msg);
+        this.data = data;
+    }
+}
+
 function catchHandler(e: ThrowError, obj: IRequestOption | IUploadFileOption, reject: (reason?: any) => void) {
     const { type, res } = e;
 
@@ -20,7 +30,7 @@ function catchHandler(e: ThrowError, obj: IRequestOption | IUploadFileOption, re
             return reject(new Error(res.statusCode.toString()));
         } else if (type === 'logic-error') {
             let msg = errorHandler.getErrorMsg(res);
-            return reject(new Error(msg.content));
+            return reject(new ErrorWithData(msg.content, res.data));
         } else {
             // 其他js错误
             return reject(e);
